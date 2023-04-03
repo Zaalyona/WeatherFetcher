4b5961d28c6d6f71e1e51b2ada473c4832e11b01
Zaalyona <zaalyona@yandex.ru>
2023-04-03 21:23:14 +0300

    - добавлен выпадающий список выбора городов

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -1,15 +1,57 @@
 package com.example.weatherfetcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val citiesArray = arrayOf("Moscow", "Saint Petersburg", "Ekaterinburg", "Sochi")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textViewHello = findViewById<TextView>(R.id.tvHello)
-        textViewHello.text = "Hello from code!"
+        textViewHello.text = "Выберите город:"
+
+        val spinner = findViewById<Spinner>(R.id.spnWeather)
+        val arrayAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            citiesArray)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(
+                    applicationContext,
+                    "Будет показан прогноз погоды в г. Москва :)",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        val btnWeather = findViewById<Button>(R.id.btnWeather)
+        btnWeather.setOnClickListener {
+            Intent(this, WeatherActivity::class.java).also(::startActivity)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/weatherfetcher/feature/weather_screen/WeatherInteractor.kt
+++ app/src/main/java/com/example/weatherfetcher/feature/weather_screen/WeatherInteractor.kt
@@ -2,6 +2,7 @@ package com.example.weatherfetcher.feature.weather_screen
 
 import com.example.weatherfetcher.feature.weather_screen.data.WeatherRepo
 
+
 class WeatherInteractor(private val weatherRepo: WeatherRepo) {
 
     fun getWeather(): String {
